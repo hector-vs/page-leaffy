65fc232bb63ab016c84456a2660189e74aa9a306
hector-vs <hectorvieirasaldivar@gmail.com>
2023-05-19 23:33:20 -0300

    commit final

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.content', {duration : 2000
+    , rotate:{
+        x:10,
+        y:0,
+        z:0}
+    }
+);
+sr.reveal('.center', {duration : 2000});
